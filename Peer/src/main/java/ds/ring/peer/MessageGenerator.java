92de06768fddbcd6e0d2141c1d5dff599e151109
Ricardo <ricardo.cruz200230@gmail.com>
2024-01-04 22:57:59 +0000

    commit

--- Peer/src/main/java/ds/ring/peer/MessageGenerator.java
+++ Peer/src/main/java/ds/ring/peer/MessageGenerator.java
@@ -2,13 +2,11 @@ package ds.ring.peer;
 
 import ds.ring.peer.poisson.PoissonProcess;
 
-import java.util.Iterator;
 import java.util.concurrent.BlockingQueue;
 import java.lang.Thread;
 import java.util.Random;
 public class MessageGenerator implements Runnable{
 
-    static final int SAMPLES = 100;
     BlockingQueue<String> messageQueue;
 
     public MessageGenerator(BlockingQueue<String> messageQueue) {
@@ -17,7 +15,7 @@ public class MessageGenerator implements Runnable{
 
     public void run() {
         PoissonProcess pp = new PoissonProcess(4, new Random(0) );
-        for (int i = 1; i <= SAMPLES; i++) {
+        while (true) {
             double t = pp.timeForNextEvent() * 60.0 * 1000.0;
             System.out.println("next event in -> " + (int)t + " ms");
             try {
